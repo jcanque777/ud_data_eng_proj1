5b613012af4616b969b335819271b2ec4b9fc0c3
John Canque <jcanque@gmail.com>
2020-09-20 12:54:07 -0400

    put all create and drop statements

---
+++ sql_queries.py
@@ -0,0 +1,122 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+# CREATE TABLES
+
+'''
+CREATE TABLE IF NOT EXISTS songplays (songplay_id int, start_time time, user_id int, level str, \
+artist_id int, session_id int, location str, user_Agent str)
+
+'''
+
+
+songplay_table_create = ('CREATE TABLE IF NOT EXISTS songplay (songplay_id int, \
+start_time time, \
+user_id int, \
+level str, \
+artist_id int, \
+session_id int, \
+location str, \
+user_Agent str);')
+
+
+'''
+CREATE TABLE IF NOT EXISTS users (user_id int, \
+first_name str, \
+last_name str, \
+gender str, \
+level str);
+'''
+
+
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int, \
+first_name str, \
+last_name str, \
+gender str, \
+level str);""")
+
+
+'''
+CREATE TABLE IF EXISTS songs (song_id int, \
+title str, \
+artist_id int, \
+year int, \
+duration numeric;
+'''
+
+song_table_create = ("""CREATE TABLE IF EXISTS songs (song_id int, \
+title str, \
+artist_id int, \
+year int, \
+duration numeric);""")
+
+
+''' 
+CREATE TABLE IF NOT EXISTS artists (artist_id int, \
+artist_name str, \
+artist_location str, \
+artist_latitude numeric, \
+aritst_longitude numeric);
+'''
+
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id int, \
+artist_name str, \
+artist_location str, \
+artist_latitude numeric, \
+aritst_longitude numeric);""")
+
+
+"""
+CREATE TABLE IF NOT EXISTS time (start_time INT PRIMARY KEY, \
+hour int, \
+day int, \
+week int, \
+month int, \
+year int, \
+weekday int);
+"""
+
+
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time INT PRIMARY KEY, \
+hour int, \
+day int, \
+week int, \
+month int, \
+year int, \
+weekday int);
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+""")
+
+
+
+user_table_insert = ("""
+""")
+
+song_table_insert = ("""
+""")
+
+artist_table_insert = ("""
+""")
+
+
+time_table_insert = ("""
+""")
+
+# FIND SONGS
+
+song_select = ("""
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
